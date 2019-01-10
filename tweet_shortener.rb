af8c0f9a895d1aa386888a7181fc47f4b17d6799
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-10 14:07:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def dictionary
+  dictionary = { 
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "b" => "be",
+    "u" => "you",
+    "@" => "at",
+    "&" => "and"
+  }
+end
+
